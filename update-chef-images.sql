a3857ecacaf7838f9385eafd60b9968c20b333ba
michalsurmanek-sketch <michalsurmanek@gmail.com>
2026-01-09 17:02:17 +0000

    login.html: desktop optimalizace - vše vidět bez scrollu

--- update-chef-images.sql
+++ update-chef-images.sql
@@ -1,60 +1,61 @@
--- Aktualizace obrázků kuchařů - profesionální fotky BEZ obličejů, zaměřené na ruce/práci v kuchyni
+-- Aktualizace obrázků kuchařů - JEDNA profesionální fotka pro všechny
+-- Detail: ruce krájející čerstvé bylinky na prkénku (neutrální, profesionální)
 -- Zkopíruj tento SQL do Supabase → SQL Editor → Run
 
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800' WHERE slug = 'klara-svobodova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1577219491135-ce391730fb4c?w=800' WHERE slug = 'radek-bures';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?w=800' WHERE slug = 'emma-kozak';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?w=800' WHERE slug = 'lucie-novakova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1571863533956-01c88e79957e?w=800' WHERE slug = 'stanislav-horak';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1559628376-f3fe5f782a2e?w=800' WHERE slug = 'viktor-stastny';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1588854337115-1c67d9247e4d?w=800' WHERE slug = 'alena-prochazka';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1590179068383-b9c69aacebd3?w=800' WHERE slug = 'tereza-markova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1565557623262-b51c2513a641?w=800' WHERE slug = 'marek-pospisil';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1581299894007-aaa50297cf16?w=800' WHERE slug = 'vojtech-kral';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'klara-svobodova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'radek-bures';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'emma-kozak';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'lucie-novakova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'stanislav-horak';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'viktor-stastny';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'alena-prochazka';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'tereza-markova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'marek-pospisil';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'vojtech-kral';
 
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1574484284002-952d92456975?w=800' WHERE slug = 'anna-nguyen';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1588854337236-6889d631faa8?w=800' WHERE slug = 'robert-maly';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1609501676725-7186f017a4b0?w=800' WHERE slug = 'jana-horakova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1617093727343-374698b1b08d?w=800' WHERE slug = 'petr-novotny';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1598103442097-8b74394b95c6?w=800' WHERE slug = 'radim-soukup';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1606787366850-de6330128bfc?w=800' WHERE slug = 'lenka-dvorakova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1505253758473-96b7015fcd40?w=800' WHERE slug = 'david-cerny';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1559847844-5315695dadae?w=800' WHERE slug = 'barbora-janosikova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1555244162-803834f70033?w=800' WHERE slug = 'tomas-vlcek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1565299543923-37dd37887442?w=800' WHERE slug = 'michaela-tesarova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'anna-nguyen';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'robert-maly';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'jana-horakova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'petr-novotny';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'radim-soukup';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'lenka-dvorakova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'david-cerny';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'barbora-janosikova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'tomas-vlcek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'michaela-tesarova';
 
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?w=800' WHERE slug = 'jan-kolar';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800' WHERE slug = 'katerina-polakova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1577219491135-ce391730fb4c?w=800' WHERE slug = 'martin-vlach';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?w=800' WHERE slug = 'zuzana-vesela';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1571863533956-01c88e79957e?w=800' WHERE slug = 'ondrej-reznicek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1559628376-f3fe5f782a2e?w=800' WHERE slug = 'nikola-braunova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1588854337115-1c67d9247e4d?w=800' WHERE slug = 'jiri-hajek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1590179068383-b9c69aacebd3?w=800' WHERE slug = 'simona-horackova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1565557623262-b51c2513a641?w=800' WHERE slug = 'filip-sedlacek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1581299894007-aaa50297cf16?w=800' WHERE slug = 'petra-urbanova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'jan-kolar';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'katerina-polakova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'martin-vlach';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'zuzana-vesela';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'ondrej-reznicek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'nikola-braunova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'jiri-hajek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'simona-horackova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'filip-sedlacek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'petra-urbanova';
 
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1574484284002-952d92456975?w=800' WHERE slug = 'adam-krejci';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1588854337236-6889d631faa8?w=800' WHERE slug = 'denisa-capkova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1609501676725-7186f017a4b0?w=800' WHERE slug = 'lukas-matejka';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1617093727343-374698b1b08d?w=800' WHERE slug = 'veronika-jelinkova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1598103442097-8b74394b95c6?w=800' WHERE slug = 'michal-beranek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1606787366850-de6330128bfc?w=800' WHERE slug = 'hana-vavrikova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1505253758473-96b7015fcd40?w=800' WHERE slug = 'roman-spacek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1559847844-5315695dadae?w=800' WHERE slug = 'adela-kucerova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1555244162-803834f70033?w=800' WHERE slug = 'daniel-kopecky';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1565299543923-37dd37887442?w=800' WHERE slug = 'kristyna-navratilova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'adam-krejci';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'denisa-capkova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'lukas-matejka';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'veronika-jelinkova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'michal-beranek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'hana-vavrikova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'roman-spacek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'adela-kucerova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'daniel-kopecky';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'kristyna-navratilova';
 
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?w=800' WHERE slug = 'pavel-mares';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800' WHERE slug = 'marketa-sikova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1577219491135-ce391730fb4c?w=800' WHERE slug = 'jakub-fiala';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1466637574441-749b8f19452f?w=800' WHERE slug = 'ivana-baranova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1571863533956-01c88e79957e?w=800' WHERE slug = 'ales-blazek';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1559628376-f3fe5f782a2e?w=800' WHERE slug = 'sarka-rychtarikova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1588854337115-1c67d9247e4d?w=800' WHERE slug = 'vaclav-pelikan';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1590179068383-b9c69aacebd3?w=800' WHERE slug = 'monika-sevcikova';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1565557623262-b51c2513a641?w=800' WHERE slug = 'richard-svoboda';
-UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1581299894007-aaa50297cf16?w=800' WHERE slug = 'gabriela-fialova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'pavel-mares';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'marketa-sikova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'jakub-fiala';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'ivana-baranova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'ales-blazek';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'sarka-rychtarikova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'vaclav-pelikan';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'monika-sevcikova';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'richard-svoboda';
+UPDATE chefs SET image_url = 'https://images.unsplash.com/photo-1556910103-1c02745aae4d?w=800&q=80' WHERE slug = 'gabriela-fialova';
 
 -- Ověření
 SELECT name, slug, image_url FROM chefs ORDER BY created_at DESC LIMIT 10;
